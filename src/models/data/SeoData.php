619b763100390ec016361e61d5e6dc0d542d8fe9
Nino Lucillo <nino@grafikbuam.de>
2023-01-18 13:27:46 +0100

    update

--- src/models/data/SeoData.php
+++ src/models/data/SeoData.php
@@ -438,7 +438,7 @@ class SeoData extends BaseObject
 
 		if ($this->_element !== null)
 		{
-			foreach (array_keys($this->_element->fields()) as $name)
+			foreach ($this->_element->attributes() as $name)
 				if ($name !== $this->_handle)
 					$variables[$name] = $this->_element->$name ?? null;
 
